632fdafb53991a0924e9a996edbb5e916f49cfec
Enrico Serrano <69224805+enricoserrano@users.noreply.github.com>
2022-08-15 17:52:26 +1200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router
+} from "react-router-dom";
 
-/*
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article";
+import SubmitArticle from "./pages/Submit-Article"; 
+/*
 import NotFoundPage from "./pages/404";
 */
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
-        <ul className="header">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/SEPractice">Select the Practice</a>
-          </li>
-          <li>
-            <a href="/SubmitArticle">Submit an Article</a>
-          </li>
-        </ul>
-        <div className="content"></div>
-      </div>
-    </Router>
-  );
-};
-
+const App = () =>  {
+    return (
+        <Router>
+        <div>
+          <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
+            <ul className="header">
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+            </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route  path="/SEPractice" component={SEPractice}/>
+            <Route  path="/SubmitArticle" component={SubmitArticle}/>
+          </div>
+        </div>
+        </Router>
+    );
+}
+ 
 export default App;
